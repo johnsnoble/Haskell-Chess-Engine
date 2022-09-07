896d2dac061d84a731ab32a94b138efa36ab5f7a
Johns <=>
2022-09-07 06:08:40 +0100

    Transposition Table Initialisation implemented

--- Table.hs
+++ Table.hs
@@ -19,8 +19,23 @@ data MyType = MyType {
 emptyType :: MyType
 emptyType = MyType { x = 2, y = True }
 
-getArr :: Int -> ST s (STUArray s Int Int)
-getArr size
+initStateTable :: Int -> ST s (STArray s Int (STUArray s Int Word64))
+initStateTable size
     = do
-        stArray <- newArray (0,size-1) nullEvalVal
-        return stArray
+        w <- newArray (0,size-1) 0
+        b <- newArray (0,size-1) 0
+        b1 <- newArray (0,size-1) 0
+        b2 <- newArray (0,size-1) 0
+        b3 <- newArray (0,size-1) 0
+        arr <- newArray (0,size-1) w
+        let bs = [w,b,b1,b2,b3]
+        forM_ [0..4] $ \i -> do
+            let board = bs !! i
+            writeArray arr i board
+        return arr
+
+initTT :: Int -> ST s (STUArray s Int Int)
+initTT size
+    = do
+        arr <- newArray (0,size-1) nullEvalVal
+        return arr
