4d1cfa73d04620e1c771c3985c4472ee076f6a38
Johns <=>
2022-09-20 23:00:06 +0100

    Hashing algorithm complete outside ST Monad

--- Eval.hs
+++ Eval.hs
@@ -9,6 +9,8 @@ import Data.Array.ST
 import Control.Monad
 import Control.Monad.ST
 
+type Window = (Int,Int)
+
 getMoves :: State -> [Move]
 getMoves state = concat ms
     where
@@ -32,6 +34,44 @@ eval d st@(_,sc,_)
     where
         ms = getMoves st
         children = [eval (d-1) (move m st)|m<-ms]
+
+bestMs :: Int -> State -> [(Int,Move)]
+bestMs d st
+    | d == 0 = []
+    | otherwise = children
+    where
+        ms = getMoves st
+        children = [(- alphabeta (move m st) (d-1) (-160,160),m)|m<-ms]
+
+ab :: Int -> State -> (Int,Move)
+ab d st
+    = f ms (-160,(0,0))
+    where
+        ms = getMoves st
+        f :: [Move] -> (Int,Move) -> (Int,Move)
+        f [] x = x
+        f (m:ms) x
+            | res > (fst x) = f ms (res,m)
+            | otherwise = f ms x
+            where
+                res = - alphabeta (move m st) (d-1) (-160,- (fst x))
+
+alphabeta :: State -> Int -> Window -> Int
+alphabeta st@(_,sc,_) d (a,b)
+    | d == 0 = sc
+    | checkWinner st = sc
+    | ms == [] = 0
+    | otherwise = ab' ms a
+    where
+        ms = getMoves st
+        ab' :: [Move] -> Int -> Int
+        ab' [] x = x
+        ab' (m:ms) x
+            | res > b = res
+            | otherwise = ab' ms x'
+            where
+                res = - alphabeta (move m st) (d-1) (-b,-x)
+                x' = max x res
 
 better :: (Int,a) -> (Int,a) -> (Int,a)
 better p@(p',_) q@(q',_)
--- Table.hs
+++ Table.hs
@@ -1,28 +1,65 @@
 module Table where
 
 import Data.Word
+import Data.Bits
 import Data.Array.Base
 import Data.Array.ST
 import Control.Monad
 import Control.Monad.ST
 import Board
-import Global hiding (State)
+import Global
+import Move
 
-type StateTable = STArray s Int (STUArray s Int Word64)
-type TransTable = STUArray s Int Int
+-- type StateTable = STArray s Int (STUArray s Int Word64)
+-- type TransTable = STUArray s Int Int
 
-hashState :: State -> Int
-hashState = undefined
+av,bv,nullv :: Int
+av = 373
+bv = 5003
+nullv = 255
 
-data MyType = MyType {
-    x :: Int,
-    y :: Bool
-}
+powers :: Int -> Int -> UArray Int Int
+powers a b = listArray (0,63) (f 64 [])
+    where
+        f :: Int -> [Int] -> [Int]
+        f p [] = f (p-1) [mod a b]
+        f p xs@(x:_)
+            | p == 0 = xs
+            | otherwise = f (p-1) ((mod (x*a) b):xs)
 
-emptyType :: MyType
-emptyType = MyType { x = 2, y = True }
+stPowers :: ST s (STUArray s Int Int)
+stPowers = do
+    arr <- newArray (0,63) 0
+    writeArray arr 0 1
+    forM_ [1..63] $ \i -> do
+        prev <- readArray arr (i-1)
+        writeArray arr i (mod (prev*373) 5003)
+    return arr
 
-initStateTable :: Int -> ST s StateTable
+{- hashState :: State -> (Int,Int,UArray Int Int) -> Int
+hashState st (a,b,pws) = foldl1 f [hash i|i<-[0..2]]
+    where
+        f :: Int -> Int -> Int
+        f x y = mod (x + y) b
+        (_,_,bbs) = st
+        bbPws = take 3 $ iterate (flip unsafeShiftL 1) 1
+        hash :: Int -> Int
+        hash n = foldl1 f [(pws!i)*(bbPws!!n)|i<-[0..63],testBit (bbs!(n+2)) i] -}
+
+modAdd :: Int -> Int -> Int
+modAdd x y = mod (x + y) bv
+
+hashState :: STUArray s Int Int -> State -> ST s Int
+hashState pws st = do
+    -- must do everything imperatively to avoid taking out st monad and back in
+    return $ foldl1 modAdd [hash i|i<-[0..2]]
+    where
+        (_,_,bbs) = st
+        bbPws = take 3 $ iterate (flip unsafeShiftL 1) 1
+        hash :: Int -> Int
+        hash n = foldl1 modAdd [(pws!i)*(bbPws!!n)|i<-[0..63],testBit (bbs!(n+2)) i]
+
+initStateTable :: Int -> ST s (STArray s Int (STUArray s Int Word64))
 initStateTable size
     = do
         w <- newArray (0,size-1) 0
@@ -30,20 +67,26 @@ initStateTable size
         b1 <- newArray (0,size-1) 0
         b2 <- newArray (0,size-1) 0
         b3 <- newArray (0,size-1) 0
-        arr <- newArray (0,size-1) w
+        arr <- newArray (0,4) w
         let bs = [w,b,b1,b2,b3]
         forM_ [0..4] $ \i -> do
             let board = bs !! i
             writeArray arr i board
         return arr
 
-initTransTable :: Int -> ST s TransTable
+initTransTable :: Int -> ST s (STUArray s Int Int)
 initTransTable size
     = do
-        arr <- newArray (0,size-1) nullEvalVal
+        arr <- newArray (0,size-1) nullv
         return arr
 
-indexOf :: State -> Int
+checkTable :: State -> ST s (STUArray s Int Int) -> ST s Bool
+checkTable st table = undefined
+--    = do
+  --      let hashed = hashState st (av,bv,powers av bv)
+        -- check if it exists in table
+
+{-indexOf :: State -> Int
 indexOf = undefined
 
 checkTable :: StateTable -> State -> ST s Bool
@@ -54,4 +97,10 @@ checkTable table state
         b <- readArray state 1
         b1 <- readArray state 2
         b2 <- readArray state 3
-        b3 <- readArray state 4
+        b3 <- readArray state 4-}
+emptys :: Int -> ST s (STUArray s Int Word64)
+emptys size
+    = do
+        arr <- newArray (0,size-1) 0
+        return arr
+
